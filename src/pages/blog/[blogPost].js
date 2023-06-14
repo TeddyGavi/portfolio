a6ed2645572f03c1f50e6e11df664dc94b9cac97
teddygavi <matc.davis@gmail.com>
2023-06-13 22:14:57 -0600

    set up dynamic single post route

--- src/pages/blog/[blogPost].js
+++ src/pages/blog/[blogPost].js
@@ -1,1 +1,29 @@
 /** specific blog post */
+
+import { getBlogPostPaths, getOneBlogPostData } from "@/lib/getBlogPostPaths";
+
+export const getStaticPaths = async () => {
+  const postPaths = await getBlogPostPaths();
+
+  return {
+    postPaths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const postData = await getOneBlogPostData(params.blogPost);
+
+  if (!postData) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      postData,
+    },
+    revalidate: 10,
+  };
+};
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,16 +1,33 @@
 /**BLOG PAGE root*/
 
+import Loading from "@/components/Loading";
 import { getBlogPosts } from "@/lib/getBlogPosts";
+import Link from "next/link";
 
 export async function getStaticProps() {
   const blog = await getBlogPosts();
-
-  return { props: { blog }, revalidate: 10 };
+  //revalidate might not be needed?
+  return { props: { blog } };
 }
 
 export default function Blog({ blog }) {
   // create a 3 column layout that is limited to 2 rows, fades in from below when more posts are availabe
+
+  if (!blog) {
+    return <Loading />;
+  }
   return (
-    <section id="blog" className="grid grid-cols-3 md:grid-cols-1"></section>
+    <section
+      id="blog"
+      className="grid grid-cols-3 grid-rows-2 gap-4 md:grid-cols-1"
+    >
+      {blog.map(({ _id, slug }) => {
+        return (
+          <div key={_id}>
+            <p>this is a post</p> <Link href={`$/{slug.current}`} />
+          </div>
+        );
+      })}
+    </section>
   );
 }
