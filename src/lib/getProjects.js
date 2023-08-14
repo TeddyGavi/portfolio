c1921e6e43e7c86d153a8e487192b2d133ed3caa
TeddyGavi <matc.davis@gmail.com>
2023-08-14 10:22:53 -0700

    re-arrange project to desc by createdAt

--- src/lib/getProjects.js
+++ src/lib/getProjects.js
@@ -1,7 +1,7 @@
 import { sanityClient } from "../../sanity.js";
 
 export const getProjects = async () => {
-  const query = /* groq */ `*[_type == "projects"]`;
+  const query = /* groq */ `*[_type == "projects"] | order(_createdAt desc)`;
   const projects = await sanityClient.fetch(query);
   return projects;
 };
