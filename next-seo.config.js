406c77a9d164efb7692968fc2be8b34e445e0627
teddygavi <matc.davis@gmail.com>
2023-10-22 16:46:18 -0600

    adding og tags

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,13 +1,29 @@
 export default SEO = {
   title: "M/D",
   description: "Web Developer Matt Davis portfolio",
-  canonical: "https://matcdavis.dev", 
+  canonical: "https://matcdavis.dev",
   openGraph: {
-    type: 'profile',
-    description: 'Welcome to the portfolio site of Matt Davis. A skilled web developer with a passion for crafting beautiful and functional websites.',
-    locale: 'en_CA',
-      url: 'https://matcdavis.dev',}
-}
+    type: "profile",
+    description:
+      "Welcome to the portfolio site of Matt Davis. A skilled web developer with a passion for crafting beautiful and functional websites.",
+    locale: "en_CA",
+    url: "https://matcdavis.dev",
+  },
+  twitter: {
+    handle: "@MatcDavis",
+    site: "@MatcDavis",
+    cardType: "summary_large_image",
+    title: "Matt Davis Front-End Web Developer",
+  },
+  openGraph: {
+    description:
+      "Welcome to the personal site of Matt Davis. A skilled web developer with a passion for crafting beautiful and functional websites. In a past life Matt was a juggler, Biologist, and Red Seal Electrician.",
+    title: "Matt Davis Front-End Web Developer",
+    images: [
+      "https://cdn.sanity.io/images/m9iwl7mr/production/2522fd2643d0eb781183d06366dec7cb8f5670b5-480x720.jpg?q=100",
+    ],
+  },
+};
 
 /* export default {
   openGraph: {
