20366dc4316f21577c385b9a9f0aae7961f2f2a7
teddygavi <matc.davis@gmail.com>
2023-10-25 00:22:04 -0600

    working on SEO and vercel config experiment

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,4 +1,4 @@
-export default SEO = {
+export const SEO = {
   title: "M/D",
   description: "Web Developer Matt Davis portfolio",
   canonical: "https://matcdavis.dev",
@@ -8,20 +8,16 @@ export default SEO = {
       "Welcome to the portfolio site of Matt Davis. A skilled web developer with a passion for crafting beautiful and functional websites.",
     locale: "en_CA",
     url: "https://matcdavis.dev",
+    title: "Matt Davis Front-End Web Developer",
+    images: [
+      "https://cdn.sanity.io/images/m9iwl7mr/production/2522fd2643d0eb781183d06366dec7cb8f5670b5-480x720.jpg?q=100",
+    ],
   },
   twitter: {
     handle: "@MatcDavis",
     site: "@MatcDavis",
     cardType: "summary_large_image",
     title: "Matt Davis Front-End Web Developer",
-  },
-  openGraph: {
-    description:
-      "Welcome to the personal site of Matt Davis. A skilled web developer with a passion for crafting beautiful and functional websites. In a past life Matt was a juggler, Biologist, and Red Seal Electrician.",
-    title: "Matt Davis Front-End Web Developer",
-    images: [
-      "https://cdn.sanity.io/images/m9iwl7mr/production/2522fd2643d0eb781183d06366dec7cb8f5670b5-480x720.jpg?q=100",
-    ],
   },
 };
 
--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+const fs = require("fs");
+const envPath = path.resolve(__dirname, ".vercel/.env.development.local");
+const env = fs.readFile(envPath, "utf8", (err, data) => {
+  if (err) return err.message;
+
+  const envObj = {};
+
+  const dataArray = data.split("\n");
+  for (let i = 0; i < dataArray.length; i++) {
+    if (dataArray[i].includes("=")) {
+      const temp = dataArray[i].split("=");
+
+      envObj[temp[0]] = temp[1].substring(1, temp[1].length - 1).trim();
+    }
+  }
+  return envObj;
+});
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["cdn.sanity.io", "source.unsplash."],
   },
+  env: env,
 };
 
 module.exports = nextConfig;
