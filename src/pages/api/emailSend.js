0e82c1cfc1f0803b4dd14bf7b95f39cd51288a13
TeddyGavi <matc.davis@gmail.com>
2023-12-07 12:36:06 -0800

    fix email issues

--- src/pages/api/emailSend.js
+++ src/pages/api/emailSend.js
@@ -1,34 +1,65 @@
-import sgMail from "@sendgrid/mail";
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// import sgMail from "@sendgrid/mail";
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+//
+// export default async function EmailSend(req, res) {
+//   if (req.method === "POST") {
+//     const { formData } = req.body;
+//
+//     const msg = {
+//       to: "matc.davis@gmail.com", // Change to your recipient
+//       from: "matc.davis@gmail.com", // Change to your verified sender
+//       subject: `Message from Portfolio site`,
+//       text: `Message from => ${formData.email}`,
+//       html: `
+//       <div>
+//         <p><strong>Subject => ${formData.subject}</strong></p>
+//         <p>Message => ${formData.message}</p>
+//       </div>
+//       `,
+//     };
+//     sgMail
+//       .send(msg)
+//       .then(() => {
+//         console.log("Email sent");
+//         res.status(200).json({
+//           status: "success",
+//           message: "Message submitted successfully",
+//         });
+//       })
+//       .catch((error) => {
+//         res.status(405).json({ status: "error", message: error });
+//       });
+//   }
+// }
+//
+// export const config = {
+//   api: {
+//     externalResolver: true,
+//   },
+// };
+//
+
+import nmTransportor from "@/utils/emailTransportor";
 
 export default async function EmailSend(req, res) {
   if (req.method === "POST") {
-    const { formData } = req.body;
+    const { email, subject, message } = req.body.formData;
 
-    const msg = {
-      to: "matc.davis@gmail.com", // Change to your recipient
-      from: "matc.davis@gmail.com", // Change to your verified sender
-      subject: `Message from Portfolio site`,
-      text: `Message from => ${formData.email}`,
-      html: `
-      <div>
-        <p><strong>Subject => ${formData.subject}</strong></p> 
-        <p>Message => ${formData.message}</p>
-      </div>
-      `,
+    const options = {
+      from: email,
+      subject,
+      message,
     };
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log("Email sent");
-        res.status(200).json({
-          status: "success",
-          message: "Message submitted successfully",
-        });
-      })
-      .catch((error) => {
-        res.status(405).json({ status: "error", message: error });
+
+    try {
+      await nmTransportor(options);
+      res.status(200).json({
+        status: "success",
+        message: "Message submitted successfully",
       });
+    } catch (error) {
+      res.status(405).json({ status: "error", message: error });
+    }
   }
 }
 
---
+++ src/utils/emailTransportor.js
@@ -0,0 +1,38 @@
+import nodemailer from "nodemailer";
+const SITE_EMAIL = process.env.SITE_EMAIL;
+const PERSONAL_EMAIL = process.env.PERSONAL_EMAIL;
+export default async function nmTransportor(options) {
+  const { from, subject, message } = options;
+
+  const transportor = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: PERSONAL_EMAIL,
+      pass: process.env.GOOGLE_APP_PASSWORD,
+    },
+  });
+
+  const emailOptions = {
+    from: SITE_EMAIL,
+    to: PERSONAL_EMAIL,
+    subject: `Contact Form Submission from ${SITE_EMAIL}`,
+    text: message,
+    html: `
+        <p>Email: ${from}</p>
+        <br>
+        <p>Subject: ${subject}</p>
+        <p>Message: ${message}</p>
+        `,
+  };
+
+  transportor.sendMail(emailOptions, (error) => {
+    if (error) {
+      throw new Error(error);
+    } else {
+      return true;
+    }
+  });
+}
